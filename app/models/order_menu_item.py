617ca1418b2c4bedbdadee18eb324cd2ab380a53
ian <iandkaneshiro@gmail.com>
2023-08-26 20:41:18 -0700

    tseting

--- app/models/order_menu_item.py
+++ app/models/order_menu_item.py
@@ -1,12 +1,13 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from .order import Order
 
 
 order_menu_items = db.Table(
     "order_menu_items",
     db.Model.metadata,
-    db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key=True),
-    db.Column('menu_item_id', db.Integer, db.ForeignKey('menu_items.id'), primary_key=True)
+    db.Column('order_id', db.Integer, db.ForeignKey(
+        add_prefix_for_prod('orders.id')), primary_key=True),
+    db.Column('menu_item_id', db.Integer, db.ForeignKey(
+        add_prefix_for_prod('menu_items.id')), primary_key=True)
 )
 
 if environment == "production":
