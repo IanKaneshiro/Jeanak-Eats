bd1eb4603dd7ca9b51b91bb3fac2c11c1e52e46f
ian <iandkaneshiro@gmail.com>
2023-08-26 21:08:51 -0700

    issues

--- app/models/menu_item.py
+++ app/models/menu_item.py
@@ -10,24 +10,27 @@ class MenuItem(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('restaurants.id')), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('restaurants.id')), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(500))
     price = db.Column(db.Float(), nullable=False)
-    category = db.Column(db.String(25), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
     dietary = db.Column(db.String(50))
     image_url = db.Column(db.String())
     created_at = db.Column(db.DateTime, default=datetime.now())
-    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+    updated_at = db.Column(
+        db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
     restaurant = db.relationship('Restaurant', back_populates='menu_items')
-    orders = db.relationship('Order', back_populates='menu_items', secondary=order_menu_items)
+    orders = db.relationship(
+        'Order', back_populates='menu_items', secondary=order_menu_items)
 
     def to_dict(self):
         return {
             'id': self.id,
             'restaurantId': self.restaurant_id,
-            'name': self.name, 
+            'name': self.name,
             'description': self.description,
             'price': self.price,
             'category': self.category,
