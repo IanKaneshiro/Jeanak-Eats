3577bdca0cac23dd2e1f3102242517d2930e1d97
ian <iandkaneshiro@gmail.com>
2023-09-07 16:10:50 -0700

    fixing errors

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { signUp } from "../../store/session";
 import { useHistory } from "react-router-dom";
 import { stateOptions, countryOptions } from "../../Resources/selectOptions";
