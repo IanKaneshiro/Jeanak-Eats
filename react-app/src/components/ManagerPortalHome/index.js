fa103a762c3c974f87aaded4aa0600992bb16557
ian <iandkaneshiro@gmail.com>
2023-09-07 17:39:40 -0700

    working on fixing styling and responsive design

--- react-app/src/components/ManagerPortalHome/index.js
+++ react-app/src/components/ManagerPortalHome/index.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { usersRestaurants } from "../../store/restaurant";
 import "./ManagerPortalHome.css";
 
 const ManagerPortalHome = () => {
+  const restaurant = useSelector(usersRestaurants);
+  const numRestaurants = restaurant.length;
+  const avgRatingScore =
+    restaurant.reduce((acc, res) => {
+      return (acc += res.avgRating);
+    }, 0) / restaurant.length;
   return (
     <div className="manager-home--container">
-      <h1>Welcome to your restaurant portal!</h1>
+      <h1>Welcome to your manager portal!</h1>
+      {numRestaurants > 0 ? (
+        <>
+          <h3>
+            You currently operate {numRestaurants}{" "}
+            {numRestaurants === 1 ? "restaurant" : "restaurants"}
+          </h3>
+          <h4>with a average rating of {avgRatingScore || 0}</h4>
+        </>
+      ) : (
+        <>
+          <h3>Looks like you have no restaurants yet</h3>
+          <Link to="/manage/restaurants/new">
+            <button className="manager-home--new">Lets get cooking!</button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
